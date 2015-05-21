b0025d2c5d5bb896cc1efc06f9d55677cfa46020
grant-shoemaker <grant.c.shoemaker@gmail.com>
2015-05-21 11:30:55 -0600

    Attempt to allow Cross-Origin requests coming from the SNL network.

--- src/PlanningPoker/Startup.cs
+++ src/PlanningPoker/Startup.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Cors.Core;
 using Microsoft.AspNet.Hosting;
 using Microsoft.AspNet.SignalR;
 using Microsoft.Framework.ConfigurationModel;
@@ -11,12 +12,25 @@ namespace PlanningPoker
     public class Startup
     {
 
+        private CorsPolicy corsPolicy()
+        {
+            var policy = new CorsPolicy();
+            policy.Headers.Add("*");
+            policy.Methods.Add("*");
+            policy.Origins.Add("*");
+            policy.SupportsCredentials = true;
+            return policy;
+        }
+
         // This method gets called by a runtime.
         // Use this method to add services to the container
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
             
+            services.AddCors();
+            services.ConfigureCors(x => x.AddPolicy("corsPolicy", corsPolicy()));
+
             services.AddSignalR(options =>
             {
                 options.Hubs.EnableDetailedErrors = true;
@@ -25,8 +39,11 @@ namespace PlanningPoker
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+
             // Configure the HTTP request pipeline.
             app.UseStaticFiles();
+
+            app.UseCors("corsPolicy");
 
             // Add MVC to the request pipeline.
             app.UseMvc(routes =>
